96e7d6e0d2d42b4931c5a72508d01bc78855959c
Lukasz Maciag <manix93@gmail.com>
2018-12-06 09:42:02 +0100

    Add basic configuration for API with simple controller.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCities() => Json(CitiesDataStore.Current.Cities);
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id) => Json(CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == id));
+    }
+}
